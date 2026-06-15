fc5676e01498e8051aaa2c6862bb5dca8e937cbf
oluoko <brianotieno586@gmail.com>
2026-06-15 12:29:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Loka - Kenyan Languages Learning App
+
+A web app that teaches users how to speak native Kenyan languages, like :-
+
+- Somali
+- Maasai
+- Kalenjin
+- Kikuyu
+- Dholuo
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-blue-700 hover:text-green-300 cursor-pointer text-9xl font-black">
+        Loka
+      </h1>
+    </div>
+  );
+}
